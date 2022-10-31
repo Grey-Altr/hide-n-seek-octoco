840e7ef05b655a10a08d554f25004ee0b5193f6d
Grey Altr <grey.sifr@gmail.com>
2022-10-31 12:41:46 -0700

    added correct container grab and face graphic add

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ function handleGuess(correctSpot, userGuess) {
     totalGuesses++;
     totalEl.textContent = totalGuesses;
     // then grab the appropriate container element for the correct guess from the DOM
+    const correctEl = document.getElementById(`${correctSpot}-container`);
     // then add the face class to that element so that the face shows up
+    correctEl.classList.add('face');
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
